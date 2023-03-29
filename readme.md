125aaa565dc526645b3e9fa29ed5edead07c4710
YsGBt <qishenpang@gmail.com>
2023-03-28 20:43:38 -0400

    feature extraction

--- readme.md
+++ readme.md
@@ -82,6 +82,71 @@
                 - 将数据集的特征 -> 字典类型
                 - DictVectorizer转换
             2. 本身拿到的数据就是字典类型
+    3. 文本特征提取
+        - 单词 作为 特征
+        - 方法1: `sklearn.feature_extraction.text.CountVectorizer(stop_words=[])` 返回词频矩阵 (统计每个样本特征词出现的个数)
+            - stop_words 停用词 (不进行统计)
+            - `CountVectorizer.fix_transform(X)` X:文本或者包含文本的字符串的可迭代对象 返回值: 返回sparse矩阵
+            - `CountVectorizer.inverse_transofrm(X)` X:array数组或者sparse矩阵 返回值: 转换之前数据格
+            - `CountVectorizer.get_feature_names()` 返回值: 单词列表
+        - 方法2: Tf-idf 文本特种提取 `TfidfVectorizer`  
+            - 主要思想: 如果某个词在某一个类别的文章中，出现的概率高，但是在其他类别的文章中出现很少，则认为此词或者短语具有良好的类别区分能力，适合用来分类。
+            - TF: 词频 (term frequenc) 指的是某一个词在文章中出现的频率
+            - IDF: 逆向文档频率 (inverse document frequency) 是一个词语普遍重要性的度量。可以由总文件数目除以包含该词语文件的数目，再将得到的商取以10为底的对数 (log<sub>10</sub>)
+            - TF-IDF (数值越大体现词越重要): <strong style="color:green">tfidf<sub>i,j</sub> = tf<sub>i,j</sub> * idf<sub>i</sub></strong>
+            - Ex. 两个词 "经济" "非常"  
+                1000篇文章 - 语料库  
+                100篇文章包含"非常"  
+                10篇文章包含"经济"
+
+                文章A (100词): 10次"经济"   
+                TF-IDF: 0.2
+                - tf: 10/100 = 0.1
+                - idf: log<sub>10</sub>(1000/10) = 2
+                
+                文章B (100词): 10次"非常"  
+                TF-IDF: 0.1
+                - tf: 10/100 = 0.1
+                - idf: log<sub>10</sub>(1000/100) = 1
+            - `sklearn.feature_extraction.text.TfidfVectorizer(stop_words=None,...)`
+                - 返回词的权重矩阵
+    4. **特征预处理**
+        - 什么是特征预处理: 通过一些转换函数将特征数据转换成更加适合算法模型的特征数据过程
+        - 归一化/标准化: 特征的单位或者大小相差较大，或者某特征的方差相比其他的特征要大出几个数量级，容易影响目标结果，所以要进行无量纲化
+        - 归一化
+            - $$ x' = {x - min \over max - min}$$
+            - $$ x'' =  x' * (mx - mi) + mi$$
+            - 作用于每一列，max为一列的最大值，min为一列的最小值，那么$x''$为最终结果，mx,mi分别为指定区间(默认值mx
+            为1, mi为0)
+            - `sklearn.preprocessing.MinMaxScaler(feature_range=(0,1),...)`
+                - `MinMaxScalar.fit_transform(X)`       
+                    - X: numpy array格式的数据[n_samples,n_features]
+                    - 返回值: 转换后形状相同的array  
+            - 缺陷: 
+                - 异常值: 最大值、最小值异常会影响归一化结果
+        - 标准化
+            - 通过对原始数据进行变换把数据变换到均值为0，标准差为1范围内
+            - $$ x' = {x - mean \over \sigma}$$
+            - `sklearn.preprocessing.StandardScaler()`
+                - 处理之后，对每列来说所有数据都聚集在均值为0附近，标准差为1
+                - `StandardScler.fit_transform(X)` 
+                    - X: numpy array格式的数据
+                    [n_samples, n_features]
+                    - 返回值: 转换后的形状相同的array
+            - 在已有样本足够多的情况下比较稳定，适合现代嘈杂大数据场景
+    5. **特征降维**
+        - 降维是指在某些限定条件下，**降低随机变量(特征)个数**，得到一组"不相关"主变量的过程
+            - 降低随机变量(特征)的个数
+            - 相关特征(correlated feature)
+                - 例如相对湿度与降雨量之间的相关
+        - 降维的两种方式：
+            - 特征选择
+            - 主成分分析
+        - 特征选择
+            - 定义: 数据中包含冗余或者相关变量(或者特征、属性、指标等)，旨在从原有特征中找出主要特征
+
+
+
 
 
 
--- sklearn_iris.py
+++ sklearn_iris.py
@@ -1,6 +1,11 @@
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction import DictVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+import jieba
+import pandas as pd
+
 
 def datasets_demo():
     """
@@ -15,10 +20,12 @@ def datasets_demo():
     print("查看目标值:\n", iris.target, iris.target.shape)
 
     # 数据集划分 (默认test_size=0.25)
-    data_train, data_test, target_train, target_test = train_test_split(iris.data, iris.target, test_size=0.2, random_state=22)
+    data_train, data_test, target_train, target_test = train_test_split(
+        iris.data, iris.target, test_size=0.2, random_state=22)
     print("训练集特征值:\n", data_train, data_train.shape)
 
     return None
+
 
 def dict_demo():
     """
@@ -27,7 +34,7 @@ def dict_demo():
     data = [{'city': '北京', 'temperature': 100},
             {'city': '上海', 'temperature': 60},
             {'city': '深圳', 'temperature': 30}]
-    
+
     # 1. 实例化一个转换器类
     transformer = DictVectorizer(sparse=False)
 
@@ -38,9 +45,151 @@ def dict_demo():
 
     return None
 
+
+def text_count_demo():
+    """
+    文本特征抽取: CountVectorizer
+    """
+    data = ["life is short, i like like python",
+            "life is to long, i dislike python"]
+    # 1. 实例化一个转换器类
+    transfer = CountVectorizer()
+
+    # 2. 调用fit_transform()
+    data_new = transfer.fit_transform(data)
+    print("data_new:\n", data_new.toarray())
+    print("特征名字", transfer.get_feature_names_out())
+
+    return None
+
+
+def text_chinese_count_demo():
+    """
+    中文文本特征抽取: CountVectorizer
+    """
+    data = ["我 爱 北京 天安门",
+            "天安门 上 太阳 升"]
+    # 1. 实例化一个转换器类
+    transfer = CountVectorizer()
+
+    # 2. 调用fit_transform()
+    data_new = transfer.fit_transform(data)
+    print("data_new:\n", data_new.toarray())
+    print("特征名字", transfer.get_feature_names_out())
+
+    return None
+
+
+def cut_word(text):
+    """
+    进行中文分词
+    """
+    return " ".join(jieba.lcut(text))
+
+
+def text_chinese_count_demo2():
+    """
+    中文文本特征抽取，自动分词
+    """
+    # 1. 将中文文本进行分词
+    data = ["一种还是一种今天很残酷，明天更残酷，后天很美好，但是大多数人死在明天晚上，看不到后天的太阳",
+            "我们看到的从很远星系来的光是在几百万年之前发出的, 在我们看到的最远的物体的情况下, 光是在80亿年前发出的。这样当我们看宇宙时, 我们是在看它的过去。",
+            "如果只用一种方式了解某样事物，你就不会真正了解它。了解事物真正含义的秘密取决于如何将其与我们所了解的其他事物相联系。 通过联系，你可将想法内化于心，从各种角度看问题，直至找到适合自己的方法。这才是思考的真谛学科之间的界限并没有那么清晰，将知识视为整体，容易将所学的知识与其他知识相联系"]
+    split_data = [cut_word(d) for d in data]
+
+    # 2. 实例化一个转换器类
+    transfer = CountVectorizer(stop_words=["一种", "只用"])
+
+    # 3. 调用fit_transform()
+    data_new = transfer.fit_transform(split_data)
+    print("data_new:\n", data_new.toarray())
+    print("特征名字", transfer.get_feature_names_out())
+
+    return None
+
+
+def tfidf_demo():
+    """
+    用TF-IDF的方法进行文本特征抽取
+    """
+    # 1. 将中文文本进行分词
+    data = ["一种还是一种今天很残酷，明天更残酷，后天很美好，但是大多数人死在明天晚上，看不到后天的太阳",
+            "我们看到的从很远星系来的光是在几百万年之前发出的, 在我们看到的最远的物体的情况下, 光是在80亿年前发出的。这样当我们看宇宙时, 我们是在看它的过去。",
+            "如果只用一种方式了解某样事物，你就不会真正了解它。了解事物真正含义的秘密取决于如何将其与我们所了解的其他事物相联系。 通过联系，你可将想法内化于心，从各种角度看问题，直至找到适合自己的方法。这才是思考的真谛学科之间的界限并没有那么清晰，将知识视为整体，容易将所学的知识与其他知识相联系"]
+    split_data = [cut_word(d) for d in data]
+
+    # 2. 实例化一个转换器类
+    transfer = TfidfVectorizer(stop_words=["一种", "只用"])
+
+    # 3. 调用fit_transform()
+    data_new = transfer.fit_transform(split_data)
+    print("data_new:\n", data_new.toarray())
+    print("特征名字", transfer.get_feature_names_out())
+
+    return None
+
+
+def minmax_demo():
+    """
+    归一化
+    """
+    # 1. 获取数据
+    data = pd.read_csv("dating.txt")
+    data = data.iloc[:, :3]
+    print(data)
+
+    # 2. 实例化一个转换器类
+    transfer = MinMaxScaler(feature_range=(2, 3))
+
+    # 3. 调用fit_transform()
+    data_new = transfer.fit_transform(data)
+    print(data_new)
+
+    return None
+
+def stand_demo():
+    """
+    标准化
+    """
+    # 1. 获取数据
+    data = pd.read_csv("dating.txt")
+    data = data.iloc[:, :3]
+    print(data)
+
+    # 2. 实例化一个转换器类
+    transfer = StandardScaler()
+
+    # 3. 调用fit_transform()
+    data_new = transfer.fit_transform(data)
+    print(data_new)
+
+    return None
+
+
 if __name__ == "__main__":
     # 代码1: sklearn数据集使用
     # datasets_demo()
 
     # 代码2: 字典特征抽取
-    dict_demo()
+    # dict_demo()
+
+    # 代码3: 文本特征抽取
+    # text_count_demo()
+
+    # 代码4: 中文文本特征抽取
+    # text_chinese_count_demo()
+
+    # 代码5: 中文文本特征抽取, 自动分词
+    # text_chinese_count_demo2()
+
+    # 代码6: 中文分词
+    # print(cut_word("我爱北京天安门"))
+
+    # 代码7: 用TF-IDF的方法进行文本特征抽取
+    # tfidf_demo()
+
+    # 代码8: 归一化
+    # minmax_demo()
+
+    # 代码9: 标准化 
+    stand_demo()
